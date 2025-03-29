108b94d2b42d726f1db0795f709cf68dfee6fc6f
Alexis <mamanialexis29@gmail.com>
2025-03-29 01:31:58 -0300

    racional falta terminar

--- TP2/Racional/Racional.java
+++ TP2/Racional/Racional.java
@@ -11,63 +11,21 @@ class Racional {
         this.numerador = numerador;
         this.denominador = denominador;
     }
-
-    private int mcd(int a, int b) {
-        if (b == 0) return a;
-        return mcd(b, a % b);
+    public int getNumerador() {
+        return numerador;
     }
-
-    private void simplificar() {
-        int divisor = mcd(numerador, denominador);
-        numerador /= divisor;
-        denominador /= divisor;
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
     }
-
-    public void suma(Racional otro) {
-        int nuevoNumerador = this.numerador * otro.denominador + otro.numerador * this.denominador;
-        int nuevoDenominador = this.denominador * otro.denominador;
-        this.numerador = nuevoNumerador;
-        this.denominador = nuevoDenominador;
-        simplificar();
-        System.out.println("Resultado de la suma: " + numerador + "/" + denominador);
+    public int getDenominador() {
+        return denominador;
     }
-
-    public void resta(Racional otro) {
-        int nuevoNumerador = this.numerador * otro.denominador - otro.numerador * this.denominador;
-        int nuevoDenominador = this.denominador * otro.denominador;
-        this.numerador = nuevoNumerador;
-        this.denominador = nuevoDenominador;
-        simplificar();
-        System.out.println("Resultado de la resta: " + numerador + "/" + denominador);
-    }
-
-    public void producto(Racional otro) {
-        int nuevoNumerador = this.numerador * otro.numerador;
-        int nuevoDenominador = this.denominador * otro.denominador;
-        this.numerador = nuevoNumerador;
-        this.denominador = nuevoDenominador;
-        simplificar();
-        System.out.println("Resultado del producto: " + numerador + "/" + denominador);
-    }
-
-    public void division(Racional otro) {
-        if (otro.numerador == 0) {
-            throw new IllegalArgumentException("No se puede dividir por cero.");
+    public void setDenominador(int denominador) {
+        if (denominador == 0) {
+            throw new IllegalArgumentException("El denominador no puede ser cero.");
         }
-        int nuevoNumerador = this.numerador * otro.denominador;
-        int nuevoDenominador = this.denominador * otro.numerador;
-        this.numerador = nuevoNumerador;
-        this.denominador = nuevoDenominador;
-        simplificar();
-        System.out.println("Resultado de la división: " + numerador + "/" + denominador);
+        this.denominador = denominador;
     }
 
-    public void potencia(int potencia) {
-        int nuevoNumerador = (int) Math.pow(this.numerador, potencia);
-        int nuevoDenominador = (int) Math.pow(this.denominador, potencia);
-        this.numerador = nuevoNumerador;
-        this.denominador = nuevoDenominador;
-        simplificar();
-        System.out.println("Resultado de la potencia: " + numerador + "/" + denominador);
-    }
+    
 }
