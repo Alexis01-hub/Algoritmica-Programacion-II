27cb16fd2d284511c907b7eccc915b435c3b7459
Alexis Mamani <mamanialexis29@gmail.com>
2025-05-10 21:44:23 -0300

    solucionar

--- TP5/ej1/PruebaStack.java
+++ TP5/ej1/PruebaStack.java
@@ -1,38 +1,38 @@
 import java.util.Stack;
-public class PruebaStack{
 
-    public static main(String[] args){
-        //pila implementada con array
+public class PruebaStack {
+
+    public static void main(String[] args) {
+        // pila implementada con array
         Stack<Integer> arrayStack = new ArrayStack<>();
         long arrayStackTime = tiempoStack(arrayStack, 1000);
         System.out.println("Tiempo de la pila implementada con array: " + arrayStackTime + " nanosegundos");
 
-        //pila implementada con lista enlazada
+        // pila implementada con lista enlazada
         Stack<Integer> linkedStack = new LinkedStack<>();
         long linkedStackTime = tiempoStack(linkedStack, 1000);
         System.out.println("Tiempo de la pila implementada con lista enlazada: " + linkedStackTime + " nanosegundos");
 
-        //pila implementada con lista doblemente enlazada
+        // pila implementada con lista doblemente enlazada
         Stack<Integer> doublyLinkedStack = new DoublyLinkedStack<>();
         long doublyLinkedStackTime = tiempoStack(doublyLinkedStack, 1000);
         System.out.println("Tiempo de la pila implementada con lista doblemente enlazada: " + doublyLinkedStackTime + " nanosegundos");
-
     }
 
-    private static long tiempoStack(Stack<Integer> stack, int n){
+    private static long tiempoStack(Stack<Integer> stack, int n) {
         long startTime = System.nanoTime();
 
-        //operacion push
+        // operación push
         for (int i = 0; i < n; i++) {
             stack.push(i);
         }
 
-        //operacion pop
+        // operación pop
         for (int i = 0; i < n; i++) {
             stack.pop();
         }
 
         long endTime = System.nanoTime();
         return endTime - startTime;
-    } 
+    }
 }
