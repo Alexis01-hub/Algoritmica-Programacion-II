c02094f9e5d8eacb8f42eb0cab16a195083304f3
Alexis <mamanialexis29@gmail.com>
2025-05-21 14:45:18 -0300

    solucion ej4

--- TP6/ej4/ArrayList.java
+++ TP6/ej4/ArrayList.java
@@ -1,5 +1,7 @@
 package ej4;
 
+import java.util.Arrays;
+
 public class ArrayList {
     private Object[] elementos;
     private int size;
@@ -14,15 +16,27 @@ public class ArrayList {
         return elementos.length;
     }
     /**
-     * Reduce la capacidad del array interno al número de elementos actuales.
+     * Agrega un elemento al final de la lista, redimensionando si es necesario.
      */
-    public void trimToSize() {
-        if (elementos.length > size) {
-            Object[] nuevo = new Object[size];
-            for (int i = 0; i < size; i++) {
+    public void add(Object elemento) {
+        if (size == elementos.length) {
+            // Duplicar la capacidad
+            Object[] nuevo = new Object[elementos.length * 2];
+            for (int i = 0; i < elementos.length; i++) {
                 nuevo[i] = elementos[i];
             }
             elementos = nuevo;
         }
+        elementos[size] = elemento;
+        size++;
     }
+    /**
+     * Reduce la capacidad del array interno al número de elementos actuales.
+     */
+    public void trimToSize() {
+        if (elementos.length > size) { // Si la capacidad del array es mayor que la cantidad de elementos, redimensiona el array para que su tamaño coincida con la cantidad de elementos
+            elementos = Arrays.copyOf(elementos, size); // redimensiona el array
+        }
+    }
+
 }
