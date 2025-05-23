c54fe20c6f926bb3152e628d9a09987de8d58767
Alexis-note <mamanialexis29@gmail.com>
2025-05-23 16:36:31 -0300

    ej 8 completar

---
+++ TP6/ej8/Empleado/TestEmpleado.java
@@ -0,0 +1,27 @@
+package Empleado;
+
+import Empleado.model.EmpleadoAsalariado;
+import Empleado.model.EmpleadoBaseMasComision;
+import Empleado.model.EmpleadoHora;
+import Empleado.model.EmpleadoPorComision;
+
+/**
+ * Clase de prueba para demostrar el uso de las clases de empleados.
+ */
+public class TestEmpleado {
+    /**
+     * Método principal para crear y mostrar diferentes tipos de empleados.
+     * @param args Argumentos de línea de comandos (no utilizados).
+     */
+    public static void main(String[] args) {
+        EmpleadoAsalariado empleadoAsalariado = new EmpleadoAsalariado("Juan", "12345678", 3000);
+        EmpleadoPorComision empleadoPorComision = new EmpleadoPorComision("Maria", "87654321", 0.1, 50000);
+        EmpleadoHora empleadoHora = new EmpleadoHora("Pedro", "11223344", 20, 160);
+        EmpleadoBaseMasComision empleadoBaseMasComision = new EmpleadoBaseMasComision("Ana", "44332211", 0.15, 60000, 2000);
+        System.out.println(empleadoAsalariado.toString());
+        System.out.println(empleadoPorComision.toString());
+        System.out.println(empleadoHora.toString());
+        System.out.println(empleadoBaseMasComision.toString());
+
+    }
+}
---
+++ TP6/ej8/Empleado/model/EmpleadoAsalariado.java
@@ -0,0 +1,48 @@
+package Empleado.model;
+
+/**
+ * Representa un empleado asalariado con un salario mensual fijo.
+ */
+public class EmpleadoAsalariado extends Empleado {
+    private double salarioMensual;
+
+    /**
+     * Constructor para inicializar un empleado asalariado.
+     * @param nombre Nombre del empleado.
+     * @param dni DNI del empleado.
+     * @param salarioMensual Salario mensual del empleado.
+     */
+    public EmpleadoAsalariado(String nombre, String dni, double salarioMensual) {
+        super(nombre, dni);
+        this.salarioMensual = salarioMensual;
+    }
+
+    /**
+     * Obtiene el salario mensual del empleado.
+     * @return Salario mensual del empleado.
+     */
+    public double getSalarioMensual() {
+        return salarioMensual;
+    }
+
+    /**
+     * Establece el salario mensual del empleado.
+     * @param salarioMensual Nuevo salario mensual del empleado.
+     */
+    public void setSalarioMensual(double salarioMensual) {
+        this.salarioMensual = salarioMensual;
+    }
+
+    /**
+     * Devuelve una representación en cadena del empleado asalariado.
+     * @return Representación en cadena del empleado asalariado.
+     */
+    @Override
+    public String toString() {
+        return "EmpleadoAsalariado{" +
+                "salarioMensual=" + salarioMensual +
+                ", nombre='" + getNombre() + '\'' +
+                ", dni='" + getDni() + '\'' +
+                '}';
+    }
+}
